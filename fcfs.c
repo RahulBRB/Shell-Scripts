063f48c18638d240db108f56c04cba9bc3958c27
Rahul <86495244+RahulBRB@users.noreply.github.com>
2023-05-24 20:55:48 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,49 +1,80 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct
+{
+    int pno, arrival, burst;
+}fcfs;
+
+void fillarray(fcfs *arr,int n)
+{
+    for (int i=0; i<n; i++)
+    {
+        printf("For Process %d:\n",i+1);
+        arr[i].pno=i+1;
+        printf("Enter the arrival time:\t");
+        scanf("%d",&arr[i].arrival);
+        fflush(stdin);
+        printf("Enter the burst time:\t");
+        scanf("%d",&arr[i].burst);
+    }
+}
+
+void sort(fcfs *arr,int n)
+{
+    fcfs tmp;
+    for (int i=0; i<n-1; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+        {
+            if (arr[i].arrival>arr[i+1].arrival)
+            {
+                tmp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=tmp;
+            }
+        }
+    }
+}
+
+void waitturn(fcfs *arr,int *wt,int *tat,int n)
+{
+    sort(arr,n);
+    int tmp;
+    wt[0]=0;
+    tat[0]=arr[0].burst;
+    for(int i=1; i<n; i++)
+    {
+        tmp=0;
+        for(int j=0; j<=i; j++) tmp+=arr[j].burst;
+        wt[i]=tmp-arr[i].burst-arr[i].arrival;
+        tat[i]=tmp-arr[i].arrival;
+    }
+}
+
+void display(fcfs *arr,int *wt,int *tat,int n)
+{
+    printf("Processes\tArrival Time\tBurst Time\n");
+    float awt=0,atat=0;
+    for (int i=0; i<n; i++)
+    {
+        printf("P%d\t\t%d\t\t%d\n",arr[i].pno,arr[i].arrival,arr[i].burst);
+        awt+=wt[i];
+        atat+=tat[i];
+    }
+    awt=awt/n;
+    atat=atat/n;
+    printf("Average Waiting Time:\t%f\n",awt);
+    printf("Average Turnaround Time:\t%f\n",atat);
+}
+
 int main()
 {
     int n;
-    printf("Enter no. of processes:");
+    printf("Enter the number of process:    ");
     scanf("%d",&n);
-    int bt[n];
-    printf("Enter burst time of processes in order:\n");
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&bt[i]);
-    }
-    int wt[n];
-    int wait=0;
-    wt[0]=0;
-    for(int i=1;i<n;i++)
-    {
-        wait=wait+bt[i-1];
-        wt[i]=wait;
-    }
-    int tt[n];
-    int turn=bt[0];
-    tt[0]=bt[0];
-    for(int i=1;i<n;i++)
-    {
-        turn=turn+bt[i];
-        tt[i]=turn;
-    }
-    float awt=0,att=0;
-    printf("Waiting time of the processes in order:\n");
-    for(int i=0;i<n;i++)
-    {
-        awt=awt+wt[i];
-        printf("%d\t",wt[i]);
-    }
-    printf("\n");
-    printf("Average Waiting Time = %f",awt/n);
-    printf("\n");
-    printf("Turnaround time of the processes in order:\n");
-    for(int i=0;i<n;i++)
-    {
-        att=att+tt[i];
-        printf("%d\t",tt[i]);
-    }
-    printf("\n");
-    printf("Average Turnaround Time = %f",att/n);
-    printf("\n");
+    fcfs arr[n]; int wt[n], tat[n];
+    fillarray(arr,n);
+    waitturn(arr,wt,tat,n);
+    display(arr,wt,tat,n);
     return 0;
 }
